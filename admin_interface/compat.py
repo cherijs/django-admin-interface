7544a4234ccc3550abb0c67a75dade0b9bb0b226
Artūrs Cirsis <arturs.cirsis@gmail.com>
2021-12-11 01:01:04 +0200

    deprecated ugettext_lazy

--- admin_interface/compat.py
+++ admin_interface/compat.py
@@ -4,7 +4,7 @@ import django
 
 if django.VERSION < (2, 0):
     from django.utils.encoding import force_text as force_str
-    from django.utils.translation import ugettext_lazy as gettext_lazy
+    from django.utils.translation import gettext_lazy as gettext_lazy
 else:
     from django.utils.encoding import force_str
     from django.utils.translation import gettext_lazy
